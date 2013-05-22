3c9a9bb7b5c7567fe9d7868a49499aa8bcd8a7b7
Kirill Berezin <enelar@develop-project.ru>
2013-05-23 03:35:18 +0400

    Heartbeat

--- main.cpp
+++ main.cpp
@@ -1,6 +1,29 @@
 #include "chat/chat.h"
 
+#include <sstream>
+#include <iostream>
+#include <time.h>
+
+std::string ToString( int i )
+{
+  std::ostringstream out;
+  out << i;
+  return out.str();
+}
+
+
 int main()
 {
+  ed::com::socket_connection s("localhost", 3030);
+  ed::gateway gw(s);
+  chat t(gw);
+  
+  while (true)
+  {
+    gw.Workflow();
+    ed::Sleep(30);
+    int time = clock();
+    t.Send(ToString(time));
+  }
   return 239;
 }
